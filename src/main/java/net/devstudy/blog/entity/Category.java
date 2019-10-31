5aeb30e20fd835ddbc4010e91151155cba74ab10
Жилич Александр Иванович <alexander.zhilich@btc.info>
2019-10-31 15:27:07 +0300

    Entities and model for lesson 11

---
+++ src/main/java/net/devstudy/blog/entity/Category.java
@@ -0,0 +1,29 @@
+package net.devstudy.blog.entity;
+
+public class Category extends AbstractEntity<Integer> {
+
+	private static final long serialVersionUID = -2731154125911204418L;
+	
+	private String name;
+	private String url;
+	private int articles;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getArticles() {
+		return articles;
+	}
+	public void setArticles(int articles) {
+		this.articles = articles;
+	}
+	
+}
